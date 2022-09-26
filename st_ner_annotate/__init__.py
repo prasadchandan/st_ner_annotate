7b14491589c10a4a688a65576175b53d41aaaeeb
Jeesu-Jung <jisu.jung5@gmail.com>
2022-09-26 18:59:34 +0900

    add pagenation

--- st_ner_annotate/__init__.py
+++ st_ner_annotate/__init__.py
@@ -1,8 +1,16 @@
 import os
+import json
 import spacy
+import pandas as pd
+import streamlit as st
 import streamlit.components.v1 as components
 
-_RELEASE = True
+_RELEASE = False
+st.session_state.update(st.session_state)
+if 'page_number' not in st.session_state:
+    st.session_state.page_number = 0
+with open('../data/test.food.cml.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
 
 if not _RELEASE:
     _component_func = components.declare_component(
@@ -40,34 +48,62 @@ def st_ner_annotate(label, text, ents, key=None):
     return component_value
 
 
+def get_label_entities(doc, wordlist, label):
+    result = []
+    for word in wordlist:
+        if 'wizard' in word.lower() or 'apprentice' in word.lower():
+            continue
+        result.append({'start': doc.find(word), 'end': doc.find(word) + len(word), 'label': label, 'word': word})
+    return result
+
+
 # app: `$ streamlit run my_component/__init__.py`
 if not _RELEASE:
-    import streamlit as st
+    st.markdown("# Demonstrating use of Next button with Session State")
 
-    st.title("Named entity recognition demo")
-    text = """Manhattan traces its origins to a trading post founded by colonists 
-    from the Dutch Republic in 1624 on Lower Manhattan; the post was named New 
-    Amsterdam in 1626. Manhattan is historically documented to have been purchased 
-    by Dutch colonists from Native Americans in 1626 for 60 guilders, which equals 
-    roughly $1059 in current terms. The territory and its surroundings came under 
-    English control in 1664 and were renamed New York after King Charles II of 
-    England granted the lands to his brother, the Duke of York. New York, based 
-    in present-day Manhattan, served as the capital of the United States from 1785 
-    until 1790. The Statue of Liberty greeted millions of immigrants as they came 
-    to America by ship in the late 19th century and is a world symbol of the United 
-    States and its ideals of liberty and peace. Manhattan became a borough during 
-    the consolidation of New York City in 1898. 
-    """
+    # A variable to keep track of which product we are currently displaying
+    # session_state = st.session_state.get(page_number=0)
 
-#     nlp = spacy.load("en_core_web_sm")
-#     entity_labels = nlp.get_pipe('ner').labels
+    last_page = len(data)
 
-#     doc = nlp(text)
-#     ents = doc.to_json()['ents']
+    # Add a next button and a previous button
+    # data_id = st.text_input('Data ID', 0)
+    # data_id = int(data_id)
 
+    prev, _, next = st.columns([1, 10, 1])
+    # if data_id > last_page:
+    #     st.session_state.page_number = 0
+    # else:
+    #     st.session_state.page_number = data_id
+    if next.button("Next"):
+        if st.session_state["page_number"] + 1 > last_page:
+            st.session_state.page_number = 0
+        else:
+            st.session_state.page_number += 1
+
+    if prev.button("Previous"):
+
+        if st.session_state["page_number"] - 1 < 0:
+            st.session_state.page_number = last_page
+        else:
+            st.session_state.page_number -= 1
+
+    # Get start and end indices of the next page of the dataframe
+    data_id = st.session_state["page_number"]
+
+    # Index into the sub dataframe
+    # sub_df = data[data_id]
+
+    # data_id = st.text_input('Data ID', 0)
+    # st.write('Document ID is', data_id)
+    # data_id = int(data_id)
+    st.title("CML tagging demo")
+    text = data[data_id]['dialog']
+    # PP_ents = [{} for word in data[data_id]['sent_index']]
+    WP_ents = get_label_entities(text, data[data_id]['keywords']['positive'], 'WP')
+    print(text)
     entity_labels = ['WP', 'PP']
-    ents = [{'start': 0, 'end': 9, 'label': 'WP'}]
-    
+    ents = WP_ents if len(WP_ents) <= 10 else WP_ents[:10]
     current_entity_type = st.selectbox("Mark for Entity Type", entity_labels)
-    entities = st_ner_annotate(current_entity_type, text, ents, key=42)
-    st.json(entities)
+    entities = st_ner_annotate(current_entity_type, text, ents)
+    # st.json(entities)
